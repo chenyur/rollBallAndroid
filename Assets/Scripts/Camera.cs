2d983851917b5789ea4bc21b68d320b75ae2853c
Chenyu Ren <genenius@gmail.com>
2017-06-13 03:07:34 -0400

    Version 1.0

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+	public GameObject player;
+	public Rigidbody playerRB;
+	public float speedOffsetMultiplier = 0.5f;
+	public float crashThreshold = 1f;
+	public float offsetChangeInFrames = 10;
+	private Vector3 offset;
+	private Vector3 velocityOffset;
+	private float velocity;
+	private float oldVelocity;
+
+	void Start () {
+		offset = transform.position - player.transform.position;
+		oldVelocity = playerRB.velocity.magnitude;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocity = playerRB.velocity.magnitude;
+		if ((oldVelocity - velocity) > crashThreshold) {
+			velocity = oldVelocity - oldVelocity / offsetChangeInFrames;
+		}
+		velocityOffset = new Vector3 (0, velocity, -velocity);
+		transform.position = player.transform.position + offset + velocityOffset * speedOffsetMultiplier;
+		oldVelocity = velocity;
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+
+public class Movement : MonoBehaviour {
+	// Update is called once per frame
+
+	private Rigidbody rb;
+	public float speed = 1;
+	public float bounceHeight = 50;
+	public float drag = 1f;
+	public float crashThreshold = 1f;
+	public AudioSource bounceAudio;
+
+
+	float oldVelocity;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		oldVelocity = rb.velocity.magnitude;
+		bounceAudio = GetComponent<AudioSource>();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit(); 
+		}
+
+		Vector3 bounce = new Vector3 (0.0f, bounceHeight, 0.0f);
+
+		rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+
+		if ((oldVelocity - rb.velocity.magnitude) > crashThreshold) {
+			print ("bounced!");
+			rb.AddForce (bounce);
+			bounceAudio.Play ();
+			Handheld.Vibrate ();
+		}
+		oldVelocity = rb.velocity.magnitude;
+	}
+	void FixedUpdate () {
+		float moveHorizontal = Input.acceleration.x;
+		float moveVertical = Input.acceleration.y;
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+}
